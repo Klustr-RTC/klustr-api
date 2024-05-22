f92f074d45b1dda30f1b8fafda28427a093cd666
Rushi_0508 <rushigandhi14@gmail.com>
2024-05-22 12:33:08 +0530

    Completed User related operations

--- Dtos/User/UserDto.cs
+++ Dtos/User/UserDto.cs
@@ -1,23 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Klustr_api.Models
+namespace Klustr_api.Dtos.User
 {
-    public class User
+    public class UserDto
     {
-        [Key]
         public Guid Id { get; set; }
-
-        [Required, MaxLength(50)]
         public string Username { get; set; } = string.Empty;
-
-        [Required, MaxLength(100)]
         public string Email { get; set; } = string.Empty;
-
-        public string? PasswordHash { get; set; }
         public string Avatar { get; set; } = string.Empty;
     }
 }
