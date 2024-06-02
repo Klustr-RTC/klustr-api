f8e9c403fbe6e6d255b0d8ea6c43e3445b109bae
Nilesh darji <nilesh.28.2003@gmail.com>
2024-06-02 12:49:03 +0530

    Fixing Deployment

--- Program.cs
+++ Program.cs
@@ -102,11 +102,11 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseCors("default");
 // app.UseHttpsRedirection();
