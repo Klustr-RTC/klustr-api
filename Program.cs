2dcba94defdb4b5fe8fbe09b0e8545c4607f87b8
Nilesh darji <nilesh.28.2003@gmail.com>
2024-06-02 11:18:31 +0530

    added docker file

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+Environment.GetEnvironmentVariables().Keys.Cast<string>().ToList().ForEach(key =>
+{
+    Console.WriteLine($"{key} : {Environment.GetEnvironmentVariable(key)}");
+});
 builder.Configuration.AddDotNetEnv(
     ".env", LoadOptions.TraversePath()
 );
